1675c516a79cec957f3d5e9df2451a50ddfa40e8
zhongzhaolong <1396997129@qq.com>
2018-10-18 00:00:08 +0800

    添加bootstrap

--- src/main/java/com/bj/cuser/controller/CUserController.java
+++ src/main/java/com/bj/cuser/controller/CUserController.java
@@ -1,20 +1,15 @@
 package com.bj.cuser.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.bj.cuser.entity.CUser;
 import com.bj.cuser.service.CUserService;
-import org.mortbay.util.ajax.JSONObjectConvertor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by zhongzhaolong on 2018/10/16.
@@ -34,9 +29,6 @@ public class CUserController {
     @RequestMapping("/getUserInfo")
     @ResponseBody public List<CUser> getUserInfo(HttpServletRequest request, HttpServletResponse response){
         List<CUser> userList=userService.findall();
-        //将获取到的数据存入ModelMap中，以便页面取出
-        Map<String,Object> ret=new HashMap<String,Object>();
-        ret.put("users",userList);
         return userList;
     }
 }
--- src/main/webapp/login/login.jsp
+++ src/main/webapp/login/login.jsp
@@ -10,20 +10,22 @@
     String basePath=request.getContextPath();
 %>
 <script type="text/javascript">
-    var basePath="";
+    var basePath="<%=basePath%>";
 </script>
 <html>
 <head>
+    <link rel="stylesheet" href="../resource/plugin/bootstrap-4.1.3-dist/css/bootstrap.min.css">
     <title>登陆</title>
 </head>
 <body>
 <form action="<%=basePath%>/cuser/find" method="post">
     <p>First name: <input type="text" name="fname" /></p>
     <p>Last name: <input type="text" name="lname" /></p>
-    <input type="登陆" value="Submit" />
+    <input type="submit" value="登陆" />
 </form>
 <button class="b-login">显示用户数据</button>
 </body>
-<script type="text/javascript" src="resource/plugin/jQuery/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="../resource/plugin/jQuery/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="../resource/plugin/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="login.js"></script>
 </html>
